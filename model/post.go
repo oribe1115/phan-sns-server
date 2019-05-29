8662514851124e76c020e09c77b2bb331746a56d
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-29 11:54:50 +0900

    postする関数を作った ハンドラーはまだ

--- model/post.go
+++ model/post.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"errors"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Post struct {
@@ -13,6 +16,37 @@ type Post struct {
 	CoopStatus
 }
 
+type NewPostData struct {
+	UserID  string `json:"user_name"`
+	Content string `json:"content"`
+	DateInGame
+	CoopStatus
+}
+
 func CreatePostsTable() {
 	db.CreateTable(&Post{})
+}
+
+func NewPost(postedData NewPostData) error {
+	newPost := Post{}
+	newPost.UserID = postedData.UserID
+	newPost.Content = postedData.Content
+	newPost.DateInGame = postedData.DateInGame
+	newPost.CoopStatus = postedData.CoopStatus
+
+	u, err := uuid.NewRandom()
+	if err != nil {
+		return errors.New("faild to make uuid")
+	}
+	newPost.PostID = u.String()
+
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	newPost.CreatedAt = time.Now().In(jst)
+
+	errDB := db.Table("posts").Create(&newPost)
+	if errDB.Error != nil {
+		return errors.New("faild to post to DB")
+	}
+
+	return nil
 }
