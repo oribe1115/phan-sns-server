546469473d9e5ab87208dde941def265c9cc6fe2
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-29 11:08:47 +0900

    post用のファイル、構造体を作った

---
+++ model/post.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+)
+
+type Post struct {
+	UserID    string    `json:"user_name"`
+	PostID    string    `json:"post_id"`
+	CreatedAt time.Time `json:"created_at"`
+	Content   string    `json:"content"`
+	DateInGame
+	CoopStatus
+}
